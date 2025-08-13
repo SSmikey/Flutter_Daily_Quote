8fe136ba208b2c34f0fc5c9903d4afa0b579f860
SSmikey <Seangnon09@gmail.com>
2025-08-13 23:57:12 +0700

    clean main translate

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'providers/quote_provider.dart';
 import 'pages/home_page.dart';
 import 'pages/categories_page.dart';
 import 'pages/favorites_page.dart';
-import 'pages/translate_page.dart';
 
 void main() {
   runApp(
@@ -101,7 +100,6 @@ class _MainNavigationState extends State<MainNavigation> {
     HomePage(),
     CategoriesPage(),
     FavoritesPage(),
-    TranslatePage(originalQuote:''),
   ];
 
   void _onItemTapped(int index) {
@@ -132,10 +130,6 @@ class _MainNavigationState extends State<MainNavigation> {
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
             label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Translate',
           ),
         ],
       ),
